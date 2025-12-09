acefd44bc146ffcd6257be5cef9f49fa4d435801
신성무 <ongsya@gmail.com>
2025-12-09 14:54:37 +0900

    api path 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import {UsersService} from "./users.service";
 import {User} from "./user.entity";
 import {CreateUserDto} from "./dto/create-user.dto";
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {
     }
